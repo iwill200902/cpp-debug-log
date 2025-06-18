5ddf0439f329739f5b3cec94d07b28a7aaf2d8f6
Motoki Kajiki <94942069+iwill200902@users.noreply.github.com>
2025-06-19 01:09:04 +0900

    引数がない時も使用できるように修正

--- debug_log/debug_log.hpp
+++ debug_log/debug_log.hpp
@@ -1,18 +1,16 @@
-
 #pragma once
 #include <iostream>
+#include <string>
 
-inline void debug_log_internal(const char* file, int line, char* message ) {
+inline void debug_log_internal(const char* file, int line, const std::string& message) {
     static int count = 0;
     std::cout << "Debug Log No. " << ++count
               << " (at " << file << ":" << line << ")" << std::endl;
-    std::cout << "Message: " << message << std::endl;
+    if (!message.empty()) {
+        std::cout << "Message: " << message << std::endl;
+    }
     std::cout << "----------------------------------------" << std::endl;
 }
 
-//オーバーロード for std::string
-inline void debug_log_internal(const char* file, int line, const std::string& message) {
-    debug_log_internal(file, line, message.c_str());
-}
-
-#define debug_log(msg) debug_log_internal(__FILE__, __LINE__, msg)
+// マクロで __FILE__, __LINE__ を自動で埋め込む
+#define debug_log(...) debug_log_internal(__FILE__, __LINE__, ##__VA_ARGS__)
